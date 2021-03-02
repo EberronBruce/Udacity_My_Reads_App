b804377a83b658131e0a99e09130ad4d68bed7dd
Bruce <bruce@redravencomputing.com>
2021-03-01 18:56:59 -0700

    Adding some routing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import BookShelves from './BookShelves';
 import { stringBookStatus } from './utils/Helpers';
+import { Route } from 'react-router-dom';
+import AddButton from './AddButton';
+import SearchBooks from './SearchBooks';
 
 
 class BooksApp extends Component {
   render(){
     return(
       <div className="App">
-        <BookShelves shelfTitles={[stringBookStatus.CURRENTLY_READING, stringBookStatus.WANT_TO_READ, stringBookStatus.READ]}/>
-        <div className="open-search">
-          <button >Add a book</button>
-        </div>
+        <Route exact path='/' render={() => (
+          <div>
+            <BookShelves shelfTitles={[stringBookStatus.CURRENTLY_READING, stringBookStatus.WANT_TO_READ, stringBookStatus.READ]}/>
+            <AddButton />
+          </div>
+        )} />
+        <Route path='/add' render={() => (
+          <SearchBooks />
+        )} />
       </div>
     )
   }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,1 +1,15 @@
-<p>Search Books</p>
+import React, { Component } from 'react';
+
+
+
+class SearchBooks extends Component {
+  render(){
+    return(
+      <div>
+        <p>Search Books</p>
+      </div>
+    )
+  }
+}
+
+export default SearchBooks;
